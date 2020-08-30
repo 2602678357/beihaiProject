7feebc00a73acc5fc05ddf68d0ee41c1126fa307
2602678357 <2602678357@qq.com>
2020-08-30 20:50:04 +0800

    手机验证码注册

---
+++ src/components/library/SearchBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div style="margin-bottom: 30px;display:flex;just;justify-content: center;align-items:
+center">
+        <el-input @keyup.enter.native="searchClick"
+        placeholder="通过书名或作者搜索"
+        prefix-icon="el-icon-search"
+        size="small"
+        style="width: 400px; margin-right: 10px;"
+        v-model="keywords">
+        </el-input>
+        <el-button type="primary" icon="el-icon-search" @click="searchClick
+"></el-button>
+    </div>
+</template>
+<script>
+export default {
+  name: 'SearchBar',
+  data () {
+    return {
+      keywords: '',
+      books: [],
+      cardLoading: []
+    }
+  },
+  methods: {
+    searchClick () {
+      this.$emit('onSearch')
+    }
+  }
+}
+</script>
